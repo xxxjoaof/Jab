c9471227d2f9976d8630126e3e22d59ffb0b0332
João França <joaofrancavvz@outlook.com>
2023-04-27 21:55:27 -0300

    okay
    okay

---
+++ jab/jab/jabApp.swift
@@ -0,0 +1,17 @@
+//
+//  jabApp.swift
+//  jab
+//
+//  Created by João Pedro França on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct jabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
