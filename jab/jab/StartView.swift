385c9f81f52b5970ca3b9d8c9b3f21d0bd82dc58
Victor pires e silva <victor._pires@hotmail.com>
2023-05-02 21:37:15 -0300

    Tela de Start
    Tela 1 Start

--- jab/jab/StartView.swift
+++ jab/jab/StartView.swift
@@ -7,31 +7,56 @@
 
 import SwiftUI
 
-struct StartView: View {
+struct LoadingView: View {
     var body: some View {
-        NavigationView{
             ZStack{
                 Color("#F1F5F4")
                     .ignoresSafeArea()
                 
-                
                 VStack{
                     //Logo Inicial do aplicativo
                     Image("LogoStart")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
                     
+                    ProgressView()
                     //Função do Loading para ir para a a tela de usuário (Estudar como fazer isso)
                     
                 }
             }
-        }
     }
 }
 
-struct StartView_Previews: PreviewProvider {
-    static var previews: some View {
-        StartView()
+struct ContentView: View {
+    @State var isLoading = true
+    
+    var body: some View {
+        if isLoading {
+            LoadingView()
+                .onAppear {
+                    
+                    // Carregando com tempo de 3 segundos
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        isLoading = false
+                    }
+                }
+        } else {
+            // Código do login
+            ZStack{
+                Color("#F1F5F4")
+                    .ignoresSafeArea()
+                
+                VStack{
+                    //Logo Inicial do aplicativo
+                    Image("LogoStart")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                    
+                    
+                }
+            }
+        }
     }
 }
